23bc1b22ced1be514962e16797ccfbd306a7ccda
jakeaiton <jakejakeaitonaiton@gmail.com>
2018-04-27 14:17:40 -0400

    added arduino code

---
+++ arduino/sketch_apr11a-banana.ino
@@ -0,0 +1,26 @@
+const int ledPin = 13;
+const int soundPin = A0;
+
+void setup()
+{
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  int value = analogRead(soundPin);
+  Serial.println(value);
+  if(value > 700)
+  {
+    digitalWrite(ledPin,HIGH);
+    delay(3000);
+  }
+  else
+  {
+    digitalWrite(ledPin,LOW);
+        //delay(100);
+
+  }
+}
